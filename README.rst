c93ce2627f90010b4e5e32e8cb6ef977f05b3266
Allen McGehee <amcgehee@beyondtrust.com>
2021-02-16 11:58:42 -0600

    New repository with patches to make it work again

---
+++ README.rst
@@ -0,0 +1,5 @@
+Description
+===========
+
+Socket server and client for work with JSON through asyncio
+It works only on python3
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import socketio
+
+setup(
+    name='socketio',
+    version=socketio.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    install_requires=[
+        'netifaces==0.10.6'
+    ],
+    author="EvgrDan",
+    url="http://zvonobot.ru"
+)
